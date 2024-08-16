a7b75a0b5c9fe067ea8645e984e27f57b0de57fd
Sloan Gauthier <sloangauthier@gmail.com>
2024-08-16 15:54:11 +0200

    fetch get userData

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
 import dataRoutes from "./data.routes";
+import userRoutes from "./user.routes";
+import { join } from "path";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/data", dataRoutes);
+router.use("/user", userRoutes);
 
 export default router;
