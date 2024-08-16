87acb0f080ffc47fd44aa040ebf79cd370964c62
Sloan Gauthier <sloangauthier@gmail.com>
2024-08-16 15:01:44 +0200

    delete Flight Log components for desktop and mobile

--- frontend/src/components/FlightLog.tsx
+++ frontend/src/components/FlightLog.tsx
@@ -1,12 +1,11 @@
-import TableFlightLogScreen from "./TableFlightLogScreen";
-import TableFlightLogMobile from "./TableFlightLogMobile";
-
 const FlightLog = ({ username = "Sloan" }) => {
   return (
     <div>
       <h2 className="p-6 text-center font-medium text-xl ">Carnet de vol de {username}</h2>
-      <TableFlightLogScreen />
-      <TableFlightLogMobile />
+      <div className="hidden lg:flex">
+        <h1>Screen</h1>
+      </div>
+      <div className="flex flex-col space-y-4 lg:hidden">Mobile</div>
     </div>
   );
 };
--- frontend/src/components/TableFlightLogMobile.tsx
+++
@@ -1,5 +0,0 @@
-const TableFlightLogMobile = () => {
-  return <div className="flex flex-col space-y-4"></div>;
-};
-
-export default TableFlightLogMobile;
