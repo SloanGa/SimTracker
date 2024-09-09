262e3bd4acf173a4bbb5de6c2d03c86639303d47
Sloan Gauthier <sloangauthier@gmail.com>
2024-09-09 16:43:27 +0200

    page error

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -4,9 +4,14 @@ const Error = () => {
   return (
     <div>
       <Nav />
-      <div className="w-full flex flex-col items-center mt-5">
-        <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you're looking for doesn't exist.</p>
+      <div className="w-full flex flex-col items-center mt-5 gap-8 pt-10">
+        <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+        <p className="text-xl text-center">
+          SimTracker-68KA{" "}
+          <a className="font-bold text-btn text-2xl" href="/">
+            Go Around !
+          </a>
+        </p>
       </div>
     </div>
   );
